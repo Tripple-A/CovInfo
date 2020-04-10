df9bac3dbb838af61e1ef942b09ee02ca4c0a2ee
Tripple-A <biodun9@gmail.com>
2020-04-10 18:54:42 +0100

    removed all default src files and created an api.js file for apis

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import track from './apis'
+
 
 function App() {
-  const { NovelCovid } = require('novelcovid');
- 
-   const track = new NovelCovid();
-   console.log(track.all())
+  console.log(track.all())
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>CovInfo</h2>
     </div>
   );
 }
