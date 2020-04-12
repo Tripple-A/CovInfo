8923f2fa4d86cf3e9569af2eecc42b8a0be11f46
Tripple-A <biodun9@gmail.com>
2020-04-12 23:07:33 +0100

    updated read.me

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { FILTER } from '../actions';
@@ -7,14 +7,12 @@ const mapDispatchToProps = dispatch => ({
   filter: word => dispatch(FILTER(word)),
 });
 const SearchBar = ({ filter }) => {
-  const [inputValue, setInputValue] = useState('');
   const updateFilter = e => {
-    setInputValue(e.target.value);
-    filter(inputValue);
+    filter(e.target.value);
   };
   return (
     <div>
-      <input value={inputValue} data-testid="input" placeholder="Search for a country here" onChange={e => updateFilter(e)} />
+      <input data-testid="input" placeholder="Search for a country here" onChange={e => updateFilter(e)} />
     </div>
   );
 };
